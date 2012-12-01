ba1c30ec7e41462197345f4edf5413f14e80c533
Yannick Warnier <yannick.warnier@beeznest.com>
2012-12-01 02:31:20 -0500

    Translated to Spanish and reformated config.js a little

--- config.js
+++ config.js
@@ -1,23 +1,22 @@
 var Settings = {
-	Logo			: "iflogo.jpg",	// URL to logo image 
-	MinimumDlSpeed	: 900,			// Required download speed in kbit/s
-    MinimumUlSpeed  : 300,          // Required upload speed in kbit/s
-	MaximumTime		: 180,			// Required response time (milliseconds)
-	RequestCount	: 20,			// Number of requests to perform
-	Debug			: true			// true, if detailed output of the test run needs to be shown, otherwise false
+  Logo            : "iflogo.jpg",	// URL to logo image 
+  MinimumDlSpeed  : 100,			// Required download speed in kbit/s
+  MinimumUlSpeed  : 300,          // Required upload speed in kbit/s
+  MaximumTime     : 180,			// Required response time (milliseconds)
+  RequestCount    : 20,			// Number of requests to perform
+  Debug           : true			// true, if detailed output of the test run needs to be shown, otherwise false
 };
 var Strings = {
-    StartTest		: "Start Test",
-	TestInProgress	: "Test is in progress, please wait...",
-	Title			: "Internet Connection Speed Test",
-	SubTitle		: "This web page allows you to test the speed of your internet connection between your computer and Infinite Family.",
-	TestFailed		: {
-		Title	: "We're sorry, your internet connection does not meet our standard.",
-		Message : "(If this was your first test try to run the test again in a couple of hours)"
-	},
-	TestPassed		: {
-		Title	: "Congratulations, your internet connection meets our standard.",
-		Message : ""
-	}
+  StartTest       : "Iniciar prueba",
+  TestInProgress  : "La prueba se está ejecutando, espere por favor...",
+  Title           : "Prueba de celeridad de conexión",
+  SubTitle        : "Esta página le permite probar la celeridad de su conexión hacia el servidor.",
+  TestFailed      : {
+     Title   : "Lo sentimos, su conexión es algo lenta y podría no ser suficiente para los objectivos de esta aplicación.",
+     Message : "(Si esto fue su primera prueba, intente correrla nuevamente en un par de horas)"
+  },
+  TestPassed : {
+    Title  : "Felicidades, su conexión cumple con los requerimientos.",
+    Message : ""
+  }
 };
-
